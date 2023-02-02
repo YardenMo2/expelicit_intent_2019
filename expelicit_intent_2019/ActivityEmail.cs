fe985ddaaa01c4029ecc677a15e443b404335dd1
student <student@A26-11>
2023-02-02 13:23:01 +0200

    Add project files.

---
+++ expelicit_intent_2019/ActivityEmail.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace expelicit_intent_2019
+{
+    [Activity(Label = "ActivityEmail")]
+    public class ActivityEmail : Activity
+    {
+        private EditText etmail;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_email);
+            // Create your application here
+            initializeViews();
+
+        }
+
+        private void initializeViews()
+        {
+           
+        }
+    }
+}
---
+++ expelicit_intent_2019/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+
+namespace expelicit_intent_2019
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button btndial;
+        private Button btnweb;
+        private Button btbyoutube;
+        private Button btnemail;
+        private Button btnsms;
+        private Button btnwaze;
+        private Button btnwhatsphone;
+        private Button btnwhatsnophone;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+                SetContentView(Resource.Layout.activity_main);
+            initializeViews();
+        }
+
+        private void initializeViews()
+        {
+            btndial = FindViewById<Button>(Resource.Id.btndial);
+            btnweb = FindViewById<Button>(Resource.Id.btnweb);
+            btbyoutube = FindViewById<Button>(Resource.Id.btbyoutube);
+            btnemail = FindViewById<Button>(Resource.Id.btnemail);
+            btnsms = FindViewById<Button>(Resource.Id.btnsms);
+            btnwaze = FindViewById<Button>(Resource.Id.btnwaze);
+            btnwhatsphone = FindViewById<Button>(Resource.Id.btnwhatsphone);
+            btnwhatsnophone = FindViewById<Button>(Resource.Id.btnwhatsnophone);
+
+            btnemail.Click += Btnemail_Click;
+        }
+
+        private void Btnemail_Click(object sender, EventArgs e)
+        {
+            Intent emailintent = new Intent(this, typeof(ActivityEmail));
+            StartActivity(emailintent);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            this.MenuInflater.Inflate(Resource.Menu.appmenu, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
